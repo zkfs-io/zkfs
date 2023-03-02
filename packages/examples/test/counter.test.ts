50e20c984c93cedc66479b5e151cf0c081a2fa91
dexterslabor <dexterslabor@users.noreply.github.com>
2023-03-02 20:17:57 +0100

    test(examples): update counter test to work with zkfsNode

--- packages/examples/test/counter.test.ts
+++ packages/examples/test/counter.test.ts
@@ -1,76 +1,15 @@
+/* eslint-disable max-statements */
+/* eslint-disable jest/consistent-test-it */
 /* eslint-disable no-console */
-/* eslint-disable @typescript-eslint/init-declarations */
-/* eslint-disable one-var */
-/* eslint-disable putout/putout */
-import {
-  isReady,
-  Mina,
-  PrivateKey,
-  type PublicKey,
-  AccountUpdate,
-  UInt64,
-} from 'snarkyjs';
-import { ContractApi } from '@zkfs/contract-api';
-import { ZkfsNode } from '@zkfs/node';
 /* eslint-disable jest/require-top-level-describe */
 
 import { AccountUpdate, UInt64 } from 'snarkyjs';
 
 import Counter from './counter.js';
-import PeerNodeHelper from './helpers/peerNode.js';
-import createLightClientConfig from './helpers/lightClient.js';
 import describeContract from './describeContract.js';
 
-const hasProofsEnabled = false;
-
-describe('counter', () => {
-  let deployerAccount: PublicKey,
-    deployerKey: PrivateKey,
-    senderAccount: PublicKey,
-    senderKey: PrivateKey,
-    zkAppAddress: PublicKey,
-    zkAppPrivateKey: PrivateKey,
-    zkApp: Counter,
-    contractApi: ContractApi,
-    zkfsPeerNodeId: string,
-    peerNodeHelper: PeerNodeHelper;
-
-  beforeAll(async () => {
-    await isReady;
-    peerNodeHelper = new PeerNodeHelper();
-    zkfsPeerNodeId = await peerNodeHelper.setup();
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    if (hasProofsEnabled) {
-      await Counter.compile();
-    }
-  });
-
-  beforeEach(async () => {
-    // eslint-disable-next-line @typescript-eslint/naming-convention, new-cap
-    const Local = Mina.LocalBlockchain({ proofsEnabled: hasProofsEnabled });
-    Mina.setActiveInstance(Local);
-
-    // first test account is the deployer
-    [{ privateKey: deployerKey, publicKey: deployerAccount }] =
-      Local.testAccounts;
-
-    // second test account is the sender
-    [, { privateKey: senderKey, publicKey: senderAccount }] =
-      Local.testAccounts;
-    zkAppPrivateKey = PrivateKey.random();
-    zkAppAddress = zkAppPrivateKey.toPublicKey();
-    zkApp = new Counter(zkAppAddress);
-
-    // setup zkfs node as light client
-    const zkfsConfig = await createLightClientConfig(zkfsPeerNodeId);
-    const lightClientNode = new ZkfsNode(zkfsConfig);
-    await lightClientNode.start();
-
-    contractApi = new ContractApi(lightClientNode);
-
-    await peerNodeHelper.watchAddress(zkAppAddress.toBase58());
-  });
-
+// eslint-disable-next-line jest/require-hook
+describeContract<Counter>('counter', Counter, (context) => {
   async function localDeploy() {
     const {
       deployerAccount,
@@ -78,6 +17,7 @@ describe('counter', () => {
       zkAppPrivateKey,
       zkApp,
       contractApi,
+      mockEventParser,
     } = context();
 
     const tx = await contractApi.transaction(zkApp, deployerAccount, () => {
@@ -89,18 +29,22 @@ describe('counter', () => {
     // this tx needs .sign(), because `deploy()` adds an account update
     // that requires signature authorization
     await tx.sign([deployerKey, zkAppPrivateKey]).send();
+
+    await mockEventParser();
     return tx;
   }
 
-  it('correctly updates the count state on the `Counter` smart contract', async () => {
+  it.only('correctly updates the count state on the `Counter` smart contract', async () => {
     expect.assertions(2);
 
     Error.stackTraceLimit = 1000;
 
-    const { senderAccount, senderKey, zkApp, contractApi } = context();
+    const { senderAccount, senderKey, zkApp, contractApi, mockEventParser } =
+      context();
 
     const tx0 = await localDeploy();
 
+    await contractApi.fetchOffchainState(zkApp);
     console.log('Counter.deploy() successful, initial offchain state:', {
       count: zkApp.count.get().value.toString(),
       offchainStateRootHash: zkApp.offchainStateRootHash.get().toString(),
@@ -118,7 +62,9 @@ describe('counter', () => {
 
     await tx1.prove();
     await tx1.sign([senderKey]).send();
+    await mockEventParser();
 
+    await contractApi.fetchOffchainState(zkApp);
     // eslint-disable-next-line putout/putout
     const { value: updatedCountOne } = zkApp.count.get();
 
@@ -141,23 +87,21 @@ describe('counter', () => {
       zkApp.update();
     });
 
-    await tx.prove();
-    await tx.sign([senderKey]).send();
+    await tx2.prove();
+    await tx2.sign([senderKey]).send();
+    await mockEventParser();
 
-    // helper function for testing
-    await peerNodeHelper.mockEventParser(
-      zkApp.address.toBase58(),
-      contractApi.virtualStorage
-    );
+    //await contractApi.fetchOffchainState(zkApp);
+    const { value: updatedCountTwo } = zkApp.count.get();
 
-    await contractApi.fetchOffchainState(zkApp);
-
-    const { value: updatedCount } = zkApp.count.get();
-    expect(updatedCount.toString()).toStrictEqual(UInt64.from(1).toString());
+    expect(updatedCountTwo.toString()).toStrictEqual(UInt64.from(2).toString());
 
     console.log('Counter.update() successful, new offchain state:', {
-      count: updatedCount.toString(),
+      count: updatedCountTwo.toString(),
       offchainStateRootHash: zkApp.offchainStateRootHash.get().toString(),
+      data: zkApp.virtualStorage?.data[zkApp.address.toBase58()],
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      tx: tx2.toPretty(),
     });
-  }, 30_000);
+  }, 40_000);
 });
