2174aeac72723e04f0cbd6f54b62e5981845f41a
Matej Šima <maht0rz@users.noreply.github.com>
2023-03-05 18:06:52 +0100

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   organizationName: 'zkfs-io', // Usually your GitHub org/user name.
   projectName: 'zkfs', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
